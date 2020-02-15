b8b04010a077b8dc570a37a15695045fa4c2e8a6
Mateus Leonhardt <mleonhardt91@hotmail.com>
2020-02-14 21:43:05 -0300

    Contact form styling

--- frontend/src/pages/Contact/index.js
+++ frontend/src/pages/Contact/index.js
@@ -12,33 +12,38 @@ function Contact() {
             <h2>Contato</h2>
             <p>Deixe-me uma mensagem e em breve entrarei em contato!</p>
             <form>
-                <label htmlFor="name">Nome</label>
-                <input 
-                    name="user_name" 
-                    id="user_name" 
-                    required  
-                    value={user_name}
-                    onChange={e => setUsername(e.target.value)} />
-
-                <label htmlFor="email">E-mail</label>
-                <input 
-                    type="email"
-                    name="email" 
-                    id="email" 
-                    required  
-                    value={email}
-                    onChange={e => setEmail(e.target.value)} />
-                
-                <label htmlFor="message">Mensagem</label>
-                <textarea 
-                    name="message" 
-                    id="message"
-                    rows="5" 
-                    required  
-                    onChange={e => setMessage(e.target.value)} 
-                >
-                    {message}
-                </textarea>
+                <div className="input-block">
+                    <input 
+                        name="user_name" 
+                        id="user_name" 
+                        required  
+                        value={user_name}
+                        onChange={e => setUsername(e.target.value)} />
+                    <label htmlFor="name">Nome</label>
+                </div>
+                <div className="input-block">
+                    <input 
+                        type="email"
+                        name="email" 
+                        id="email" 
+                        required  
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} />
+                    <label htmlFor="email">E-mail</label>
+                </div>
+                <div className="input-block">
+                    
+                    <textarea 
+                        name="message" 
+                        id="message"
+                        rows="5" 
+                        required  
+                        onChange={e => setMessage(e.target.value)} 
+                    >
+                        {message}
+                    </textarea>
+                    <label htmlFor="message">Mensagem</label>
+                </div>
 
                 <button type="submit">Enviar mensagem</button>
             </form>
