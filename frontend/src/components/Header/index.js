e0cea40927ab2945e8707e7c6ad952caa8404894
Mateus Leonhardt <mateus.leonhardt@conectt.com.br>
2020-03-13 17:28:33 -0300

    Finished mobile responsive

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 
 import SocialIcons from '../SocialIcons';
+import MenuButton from '../MenuButton';
 
 import './styles.css';
 
-export default function Header() {
+export default function Header(props) {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function handleMenuClick() {
+        setMenuOpen(!menuOpen);
+    }
+      
+    function handleLinkClick() {
+        setMenuOpen(false);
+    }
+
     return (
         <header id="main-header">
             <div className="links">
                 <SocialIcons className="social-icons" />
 
-                <ul className="menu">
-                    <li>
-                        <NavLink to="/" activeClassName="active" exact>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/sobre" activeClassName="active">Sobre</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contato" activeClassName="active">Contato</NavLink>
-                    </li>
-                </ul>
+                <div className={"main-menu " + (menuOpen ? "active" : '')}>
+                    <ul className="menu-list">
+                        <li>
+                            <NavLink to="/" activeClassName="active" exact onClick={()=>{handleLinkClick();}}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/sobre" activeClassName="active" onClick={()=>{handleLinkClick();}}>Sobre</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/portfolio" activeClassName="active" onClick={()=>{handleLinkClick();}}>Portfolio</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contato" activeClassName="active" onClick={()=>{handleLinkClick();}}>Contato</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                
+                <MenuButton open={menuOpen} onClick={()=>handleMenuClick()} />
             </div>            
         </header>
     );
