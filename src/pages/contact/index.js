f1afa9c0a0d2e6b6321779a15f43c4fc04c5aae4
Mateus leonhardt <mateus.leonhardt@gmail.com>
2020-04-13 10:49:28 -0300

    Updating form for submissions

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -63,7 +63,15 @@ export default function Contact({ pageContext }) {
                 <PageTitle title="Contato" />
                 <p>Deixe-me uma mensagem e em breve entrarei em contato!</p>
 
-                <Form ref={formRef} onSubmit={handleSubmit} name="contact" data-netlify="true" method="POST">
+                <Form ref={formRef} 
+                    onSubmit={handleSubmit} 
+                    name="contact" 
+                    data-netlify="true" 
+                    data-netlify-honeypot="bot-field" 
+                    method="POST"
+                >
+                    <input type="hidden" name="form-name" value="contact" />
+
                     <Input name="user_name" label="Nome*" required />
                     <Input name="email" label="E-mail*" type="email" required />
                     <Textarea name="message" label="Mensagem*" rows="10" required />
