aff2fb2fe7739891e6909893342b493dd9b1f68e
Mateus Leonhardt <mateus.leonhardt@gmail.com>
2020-10-03 03:30:40 -0300

    removed unform

--- src/components/Form/Recaptcha.js
+++ src/components/Form/Recaptcha.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 
 export default function Input({ name, ...rest }) {
-    const site_key = "6LcNqtoUAAAAAKeLBEmYpOmGD1Xo7g_XwcIpFHbn";
-    //const site_key = "6LfGyeYUAAAAAIeeVBQwqIgEuwAFNqsV95f6mL5V"; //Prod
+    //const site_key = "6LcNqtoUAAAAAKeLBEmYpOmGD1Xo7g_XwcIpFHbn";
+    const site_key = "6LfGyeYUAAAAAIeeVBQwqIgEuwAFNqsV95f6mL5V"; //Prod
 
     return (
         <div className="recaptcha">
